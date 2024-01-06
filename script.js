87b3dc0f8927bfe9db2fdd8ca5a88861409e2fca
mohamed ahmed <mahmed99786@gmail.com>
2024-01-06 05:50:12 -0500

    finished

--- script.js
+++ script.js
@@ -5,24 +5,36 @@ const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackb
 
 function search(str) {
 	let results = [];
-
 	// TODO
-
+	if (str.length) {
+	  results = fruit.filter((fruits) => {
+		return fruits.toLowerCase().includes(str.toLowerCase());
+	  });
+	}
 	return results;
-}
-
-function searchHandler(e) {
-	// TODO
-}
-
-function showSuggestions(results, inputVal) {
-
-	// TODO
-}
-
-function useSuggestion(e) {
-	// TODO
-}
-
-input.addEventListener('keyup', searchHandler);
-suggestions.addEventListener('click', useSuggestion);
+  }
+  
+  function searchHandler(e) {
+	const searchResults = search(e.target.value);
+	showSuggestions(searchResults, e.target.value);
+  }
+  
+  function showSuggestions(results, inputVal) {
+	suggestions.innerHTML = '';
+	const filteredResults = results
+	  .filter(result => result.toLowerCase().includes(inputVal.toLowerCase()))
+	  .slice(0, 4); 
+	filteredResults.forEach(result => {
+	  const li = document.createElement('li');
+	  li.textContent = result;
+	  suggestions.appendChild(li);
+	});
+  }
+  
+  function useSuggestion(e) {
+	input.value = e.target.textContent;
+	suggestions.innerHTML = '';
+  }
+  
+  input.addEventListener('keyup', searchHandler);
+  suggestions.addEventListener('click', useSuggestion);
